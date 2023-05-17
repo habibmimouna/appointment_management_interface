21719cf47836f2cbdfff2839fe499237d42418f4
habibmimouna <habibmimouna00@gmail.com>
2023-05-17 16:52:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import AllMeetUpsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import Layout from "./components/layout/Layout";
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<AllMeetUpsPage />} />
+        <Route path="/newmeetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
